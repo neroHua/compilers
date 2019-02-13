7a27ccd045516cc6336fbf1820bba6eb9b812f90
neroHua <2676337735@qq.com>
2019-02-13 17:13:43 +0800

    add regular expression exercise 3.4

--- src/main/chapter03/part01/RegularExpression01.java
+++ src/main/chapter03/part01/RegularExpression01.java
@@ -1,5 +1,15 @@
 package main.chapter03.part01;
 
+/**
+ * 
+ * 正则表达式 和有限状态自动机装换成(finite automata)
+ * 
+ * 将有限状态自动机转化成正则表达式核心思想是：将多个状态转成合并成一个可以接受的状态，在合并的过程中保持路径的连续性。
+ * 
+ * @author 滑德友
+ * @time 2019年1月8日20:34:02
+ *
+ */
 public class RegularExpression01 {
     
     public final String REGULAR_EXPRESSION_3_3_5_A = "[a-z&&[^aeiou]]*a*[a-z&&[^eiou]]*e*[a-z&&[^aiou]]*i*[a-z&&[^aeou]]*o*[a-z&&[^aeiu]]*u*[a-z&&[^aeio]]*$";
@@ -8,17 +18,44 @@ public class RegularExpression01 {
 
     public final String REGULAR_EXPRESSION_3_3_5_C = "^\\/\\*(((?!.*(\\*\\/).*).)*|([^\"]*\"[^\"]*\\*\\/[^\"]*\"[^\"]*))\\*\\/$";
     
-    public final String REGULAR_EXPRESSION_3_3_5_D = "^(?!(?:"
-            + "(?:00[0-9]*)|(?:0[1-9]*0[0-9]*)|(?:[1-9]*0[1-9]*0[0-9]*)"
-            + "(?:11[0-9]*)|(?:1[02-9]*1[0-9]*)|(?:[02-9]*1[02-9]*1[0-9]*)"
-            + "(?:22[0-9]*)|(?:2[0-13-9]*2[0-9]*)|(?:[0-13-9]*2[0-13-9]*2[0-9]*)"
-            + "(?:33[0-9]*)|(?:3[0-24-9]*3[0-9]*)|(?:[0-24-9]*3[0-24-9]*3[0-9]*)"
-            + "(?:44[0-9]*)|(?:4[0-35-9]*4[0-9]*)|(?:[0-35-9]*4[0-35-9]*4[0-9]*)"
-            + "(?:55[0-9]*)|(?:5[0-46-9]*5[0-9]*)|(?:[0-46-9]*5[0-46-9]*5[0-9]*)"
-            + "(?:66[0-9]*)|(?:6[0-57-9]*6[0-9]*)|(?:[0-57-9]*6[0-57-9]*6[0-9]*)"
-            + "(?:77[0-9]*)|(?:7[0-68-9]*7[0-9]*)|(?:[0-68-9]*7[0-68-9]*7[0-9]*)"
-            + "(?:88[0-9]*)|(?:8[0-79]*8[0-9]*)|(?:[0-79]*8[0-79]*8[0-9]*)"
-            + "(?:99[0-9]*)|(?:9[0-8]*9[0-9]*)|(?:[0-8]*9[0-8]*9[0-9]*)"
-            + "))[0-9]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_0 = "^(?:00[0-9]*)|(?:0[1-9]*0[0-9]*)|(?:[1-9]*0[1-9]*0[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_1 = "^(?:11[0-9]*)|(?:1[02-9]*1[0-9]*)|(?:[02-9]*1[02-9]*1[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_2 = "^(?:22[0-9]*)|(?:2[0-13-9]*2[0-9]*)|(?:[0-13-9]*2[0-13-9]*2[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_3 = "^(?:33[0-9]*)|(?:3[0-24-9]*3[0-9]*)|(?:[0-24-9]*3[0-24-9]*3[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_4 = "^(?:44[0-9]*)|(?:4[0-35-9]*4[0-9]*)|(?:[0-35-9]*4[0-35-9]*4[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_5 = "^(?:55[0-9]*)|(?:5[0-46-9]*5[0-9]*)|(?:[0-46-9]*5[0-46-9]*5[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_6 = "^(?:66[0-9]*)|(?:6[0-57-9]*6[0-9]*)|(?:[0-57-9]*6[0-57-9]*6[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_7 = "^(?:77[0-9]*)|(?:7[0-68-9]*7[0-9]*)|(?:[0-68-9]*7[0-68-9]*7[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_8 = "^(?:88[0-9]*)|(?:8[0-79]*8[0-9]*)|(?:[0-79]*8[0-79]*8[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_9 = "^(?:99[0-9]*)|(?:9[0-8]*9[0-9]*)|(?:[0-8]*9[0-8]*9[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_D_X = "^[0-9]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_D = "!(0|1|2|3|4|5|6|7|8|9) && X";
 
+    public final String REGULAR_EXPRESSION_3_3_5_E_0 = "^(?:000[0-9]*)|(?:00[1-9]*0[0-9]*)|(?:0[1-9]*0[1-9]*0[0-9]*)|(?:[1-9]*0[1-9]*0[1-9]*0[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_1 = "^(?:111[0-9]*)|(?:11[02-9]*1[0-9]*)|(?:1[02-9]*1[02-9]*1[0-9]*)|(?:[02-9]*1[02-9]*1[02-9]*1[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_2 = "^(?:222[0-9]*)|(?:22[0-13-9]*2[0-9]*)|(?:2[0-13-9]*2[0-13-9]*2[0-9]*)|(?:[0-13-9]*2[0-13-9]*2[0-13-9]*2[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_3 = "^(?:333[0-9]*)|(?:33[0-24-9]*3[0-9]*)|(?:3[0-24-9]*3[0-24-9]*3[0-9]*)|(?:[0-24-9]*3[0-24-9]*3[0-24-9]*3[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_4 = "^(?:444[0-9]*)|(?:44[0-35-9]*4[0-9]*)|(?:4[0-35-9]*4[0-35-9]*4[0-9]*)|(?:[0-35-9]*4[0-35-9]*4[0-35-9]*4[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_5 = "^(?:555[0-9]*)|(?:55[0-46-9]*5[0-9]*)|(?:5[0-46-9]*5[0-46-9]*5[0-9]*)|(?:[0-46-9]*5[0-46-9]*5[0-46-9]*5[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_6 = "^(?:666[0-9]*)|(?:66[0-57-9]*6[0-9]*)|(?:6[0-57-9]*6[0-57-9]*6[0-9]*)|(?:[0-57-9]*6[0-57-9]*6[0-57-9]*6[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_7 = "^(?:777[0-9]*)|(?:77[0-68-9]*7[0-9]*)|(?:7[0-68-9]*7[0-68-9]*7[0-9]*)|(?:[0-68-9]*7[0-68-9]*7[0-68-9]*7[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_8 = "^(?:888[0-9]*)|(?:88[0-79]*8[0-9]*)|(?:8[0-79]*8[0-79]*8[0-9]*)|(?:[0-79]*8[0-79]*8[0-79]*8[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_9 = "^(?:999[0-9]*)|(?:99[0-8]*9[0-9]*)|(?:9[0-8]*9[0-8]*9[0-9]*)|(?:[0-8]*9[0-8]*9[0-8]*9[0-9]*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_E_X = "^[0-9]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_E = "!(0|1|2|3|4|5|6|7|8|9) && X";
+
+    public final String REGULAR_EXPRESSION_3_3_5_F_A1 = "^b*ab*(?:b*ab*ab*)*$";
+    public final String REGULAR_EXPRESSION_3_3_5_F_A2 = "^(?:(?:b*ab*ab*)*b*ab*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_F_B1 = "^(?:(?:a*ba*ba*)*)$";
+    public final String REGULAR_EXPRESSION_3_3_5_F_X = "^[a-b]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_F = "^(?:b|a(?:aa|bb)*(?:ab|ba))(?:aa|bb|(?:ba|ab)(?:aa|bb)*(?:ab|ba))*)$";
+
+    public final String REGULAR_EXPRESSION_3_3_5_H_0 = "[ab]*(?:abb)[ab]*";
+    public final String REGULAR_EXPRESSION_3_3_5_H_X = "^[a-b]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_H = "!(0) && X";
+    
+    public final String REGULAR_EXPRESSION_3_3_5_I_0 = "(?:[ab]*a[ab]*b[ab]*b[ab]*)|(?:[ab]*b[ab]*a[ab]*b[ab]*)|(?:[ab]*b[ab]*b[ab]*a[ab]*)";
+    public final String REGULAR_EXPRESSION_3_3_5_I_X = "^[a-b]*$";
+    public final String REGULAR_EXPRESSION_3_3_5_I = "!(0) && X";
+    
 }
--- src/test/chapter03/part01/RegularExpression01Test.java
+++ src/test/chapter03/part01/RegularExpression01Test.java
@@ -53,147 +53,385 @@ public class RegularExpression01Test {
     @Test
     public void test008() {
         String s = "023";
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test009() {
         String s = "0230";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test010() {
         String s = "0203";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test011() {
         String s = "003";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test012() {
         String s = "400";
-        // 这里的结果应该是false，但是由于jdk本身的原因导致这里被我主动改成true
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test013() {
         String s = "ab4";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_0));
     }
     
     @Test
     public void test014() {
         String s = "123";
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test015() {
         String s = "1231";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test016() {
         String s = "1213";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test017() {
         String s = "113";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test018() {
         String s = "411";
-        // 这里的结果应该是false，但是由于jdk本身的原因导致这里被我主动改成true
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test019() {
         String s = "ab4";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_1));
     }
     
     @Test
     public void test020() {
         String s = "823";
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test021() {
         String s = "8238";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test022() {
         String s = "8283";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test023() {
         String s = "883";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test024() {
         String s = "488";
-        // 这里的结果应该是false，但是由于jdk本身的原因导致这里被我主动改成true
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test025() {
         String s = "ab4";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_8));
     }
     
     @Test
     public void test026() {
         String s = "923";
-        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
     
     @Test
     public void test027() {
         String s = "9239";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
     
     @Test
     public void test028() {
         String s = "9293";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
     
     @Test
     public void test029() {
         String s = "993";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
     
     @Test
     public void test030() {
         String s = "499";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
     
     @Test
     public void test031() {
         String s = "ab4";
-        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D));
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_D_9));
     }
+
+    @Test
+    public void test032() {
+        String s = "023";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test033() {
+        String s = "0230";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test034() {
+        String s = "0203";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test035() {
+        String s = "003";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test036() {
+        String s = "400";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test037() {
+        String s = "0001";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test038() {
+        String s = "0010";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test039() {
+        String s = "0100";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test040() {
+        String s = "1000";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test041() {
+        String s = "ab4";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_0));
+    }
+    
+    @Test
+    public void test042() {
+        String s = "123";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test043() {
+        String s = "1231";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test044() {
+        String s = "1213";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test045() {
+        String s = "113";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test046() {
+        String s = "411";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test047() {
+        String s = "1113";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test048() {
+        String s = "1131";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test049() {
+        String s = "1311";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test050() {
+        String s = "3111";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test051() {
+        String s = "ab4";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_1));
+    }
+    
+    @Test
+    public void test052() {
+        String s = "823";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test053() {
+        String s = "8238";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test054() {
+        String s = "8283";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test055() {
+        String s = "883";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test056() {
+        String s = "488";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test057() {
+        String s = "8883";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test058() {
+        String s = "8838";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test059() {
+        String s = "8388";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test060() {
+        String s = "3888";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test061() {
+        String s = "ab4";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_8));
+    }
+    
+    @Test
+    public void test062() {
+        String s = "923";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test063() {
+        String s = "9239";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test064() {
+        String s = "9293";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test065() {
+        String s = "993";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test066() {
+        String s = "499";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test067() {
+        String s = "9993";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test068() {
+        String s = "9939";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test069() {
+        String s = "9399";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test070() {
+        String s = "3999";
+        Assert.assertEquals(true, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
+    @Test
+    public void test071() {
+        String s = "ab4";
+        Assert.assertEquals(false, s.matches(regularExpression01.REGULAR_EXPRESSION_3_3_5_E_9));
+    }
+    
 }
